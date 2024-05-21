1343a8889fb826b8a7f9ce080a5dcb55582bb484
Jaime Sancho <jimysanchomolero@gmail.com>
2024-05-21 18:17:15 +0200

    fix: bug if question_type is not in (simple,complex)

--- app/agent/types.py
+++ app/agent/types.py
@@ -19,17 +19,17 @@ class ContextType(enum.Enum):
 
 class Output(BaseModel):
     
-    question: str
+    query: str
     reasoning: str
     question_type: str
     subject: Set[str | None]
-    valid: Optional[bool] = False
+    valid: Optional[bool] = True
     
     @model_validator(mode='before')
     def coherence_between_question_type_and_length_of_subjects(cls, values: Dict[str, Any]) -> Dict[str, Any]:
         subject, question_type = values.get('subject'), values.get('question_type')
+        if question_type not in ('simple', 'complex'):
+            return values
         if (len(subject) <= 1 and question_type != 'simple') or (len(subject) > 1 and question_type != 'complex'):
             values['valid'] = False
-        else:
-            values['valid'] = True
         return values    
