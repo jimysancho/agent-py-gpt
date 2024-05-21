ee2370ac028122d65c04657ae61b96b17ef76c36
Jaime Sancho <jimysanchomolero@gmail.com>
2024-05-21 18:15:16 +0200

    feat: add error handling logic to avoid errors

--- app/agent/multi_agent.py
+++ app/agent/multi_agent.py
@@ -1,4 +1,3 @@
-from app.prompts.prompts import SIMPLE_VS_COMPLEX, GENERAL_VS_PARTICULAR_CONTEXT
 from app.agent.agent import BaseAgent
 from app.retrievers.base import BaseRetriever
 from app.retrievers.general_retriever import GeneralRetriever
@@ -8,7 +7,12 @@ from app.agent.types import QuestionType, ContextType, Output
 
 from typing import (List, 
                     Dict, 
-                    Any)
+                    Any, 
+                    Tuple)
+
+from app.printer import Printer
+
+printer = Printer()
 
 class MultiAgent:
     
@@ -23,25 +27,66 @@ class MultiAgent:
                 ContextType.PARTICULAR: SimilarityRetriever
             }
         }
+        
+        self._default_tool = SimilarityRetriever
                 
-    async def pipeline(self, **kwargs) -> BaseRetriever | None:
+    async def pipeline(self, retries: int = 3, **kwargs) -> Tuple[BaseRetriever, Output] | None:
         """The order of actions: question_type -> context_type -> retriever"""
         
         output = None
         for agent in self._agents:
             answer = await agent.acall(**kwargs) if output is None else await agent.acall(**output.model_dump())
-            print("Agent response: ", answer)
             answer_type = agent.get_variable_of_interest(result=answer)
-            tool = self._output_to_tool[answer_type]
-            if isinstance(tool, BaseRetriever):
-                return tool
-            output = self.parse_answer_into_output(question=kwargs['query'], answer=answer)
-        
+            tool = (self._output_to_tool[answer_type] if isinstance(answer_type, QuestionType)
+                    else self._output_to_tool[QuestionType.SIMPLE][answer_type])
+            
+            try:
+                output = self._parse_answer_into_output(question=kwargs['query'], answer=answer)
+            except SyntaxError:
+                printer.print_red("Bad parsing from the llm. Trying again...")
+                answer = await agent.acall(**kwargs) if output is None else await agent.acall(**output.model_dump())
+                try:
+                    output = self._parse_answer_into_output(question=kwargs['query'], answer=answer)
+                except SyntaxError:
+                    printer.print_red("The LLM is not acting good. Try it again later.")
+                    return None
+            printer.print_blue(f"Agent reasoning response: {output.reasoning}")
+            printer.print_blue(f"Agent answer: {output.question_type} \n")
+            
+            if not output.valid:
+                printer.print_red("Not valid answer. Applying retries")
+                retry = 0
+                while retry <= retries:
+                    try:
+                        output, tool = await self._apply_retry(agent=agent, **kwargs)
+                        if output.valid:
+                            break
+                    except Exception as e:
+                        printer.print_red(f"Something went wrong in the retry: {e}")
+                        retry += 1
+                        
+                    if retry == retries:
+                        printer.print_red("Could not get a coherent response. Returning default tool...")
+                        return self._default_tool, output
+                    retry += 1
+                    
+            if tool in (SimilarityRetriever, GeneralRetriever):
+                type_of_tool = "SimilarityRetriever" if tool == SimilarityRetriever else "GeneralRetriever"
+                printer.print_green(f"Tool decided by the agent: {type_of_tool}")
+                return tool, output
+
         return None
     
     @staticmethod
-    def parse_answer_into_output(question: str, answer: Dict[str, Any] | str) -> Output:
+    def _parse_answer_into_output(question: str, answer: Dict[str, Any] | str) -> Output:
         if not isinstance(answer, dict): answer = eval(answer)
-        return Output(question=question, **answer)
-
-  
+        return Output(query=question, **answer)
+    
+    async def _apply_retry(self, agent: BaseAgent, **kwargs) -> Tuple[Output, Any]:
+        answer = await agent.acall(**kwargs)
+        printer.print_red(f"\tRetry agent response: {answer}")
+        answer_type = agent.get_variable_of_interest(result=answer)
+        tool = (self._output_to_tool[answer_type] if isinstance(answer_type, QuestionType)
+                else self._output_to_tool[QuestionType.SIMPLE][answer_type])
+        
+        return self._parse_answer_into_output(question=kwargs['query'], answer=answer), tool
