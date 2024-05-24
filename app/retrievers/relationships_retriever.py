94d9b736cdcd981a8a9398ffc9420113dc702b55
Jaime Sancho <jimysanchomolero@gmail.com>
2024-05-24 13:52:55 +0200

    feat: parent-child frequency filter

--- app/retrievers/relationships_retriever.py
+++ app/retrievers/relationships_retriever.py
@@ -1,10 +1,11 @@
-# NOTE -> create some kind of logic to determine what relationships are really useful. Maybe compare the original node text with the relationship text + query or something like that. 
-# With agents I don't think the result will be good but try it anyway. 
+# NOTE -> With agents I don't think the result will be good but try it anyway. 
 from app.database.models import Node
 from app.embeddings import HugginFaceEmbeddings
 from app.printer import Printer
 
-from typing import Dict, List, Any
+from sqlalchemy.orm import Session
+
+from typing import Dict, List
 import numpy as np
 
 printer = Printer()
@@ -22,6 +23,46 @@ class RelationShipRetriever:
         
         self._embeddings = HugginFaceEmbeddings()
         
+    def _check_if_parent_node_is_in_relationships(self, db: Session, relationships: Dict[str, List[Node]]) -> Dict[str, List[Node]]:
+        """Count the number of child nodes that appear and compare it to the actual number of child nodes a parent node has. If the number
+        is high, return the parent_node and remove the child nodes, or remove the parent node and return the child nodes"""
+        parent_node_ids = {}
+        ids = {}
+        printer.print_blue(f"\tIngoing relationships: {relationships}")
+        for rel_id, rel_nodes in relationships.items():
+            parent_node_ids[rel_id] = set()
+            ids[rel_id] = set()
+            for node in rel_nodes:
+                parent_node_id = node.parent_node_id
+                if parent_node_id:
+                    parent_node_ids[rel_id].add(parent_node_id)
+                ids[rel_id].add(node.id)
+
+        print(parent_node_ids)
+        filtered_parent_child_relationships: Dict[str, List[Node]] = {}
+        for rel_id, parent_ids in parent_node_ids.items():
+            filtered_parent_child_relationships[rel_id] = []
+            for parent_id in parent_ids:
+                childs = db.query(Node).filter(Node.parent_node_id == parent_id).all()
+                childs_retrieved = [child for child in childs if child.id in ids[rel_id]]
+                retrieved_proportion = len(childs_retrieved) / len(childs)
+                printer.print_blue(f"\tProportion of child nodes retrieved: {retrieved_proportion}")
+                if retrieved_proportion >= 0.5:
+                    parent_node = db.query(Node).filter(Node.id == parent_id).first()
+                    filtered_parent_child_relationships[rel_id].append(parent_node)
+                    nodes = relationships[rel_id]
+                    for node in nodes:
+                        if node not in childs_retrieved:
+                            filtered_parent_child_relationships[rel_id].append(node)
+                    
+                    printer.print_blue(f"\tWe have removed: {len(relationships[rel_id]) - len(filtered_parent_child_relationships[rel_id])} nodes by adding the parent.")
+                else:
+                    filtered_parent_child_relationships[rel_id] = relationships[rel_id]
+        
+        printer.print_blue(f"\tOutgoing relationships: {filtered_parent_child_relationships}")
+        return filtered_parent_child_relationships
+        
+                
     def filter_relationships(self, threshold: float) -> Dict[str, List[Node]] | Dict:
         filtered_relationships = {}
         for node in self._nodes:
